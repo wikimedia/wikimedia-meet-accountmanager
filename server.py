cff9cfe25997f50d8d320001261edc23dc2123ee
Amir Sarabadani <Ladsgroup@gmail.com>
2020-04-26 14:50:58 +0200

    Improve the workflow of creating accounts
    Right now, the ticketmaster creates the account, knowing the other
    person's user and password which is suboptimal. With this change, the
    ticketmaster generates a one-time-use token for others and they can
    create the account on their own.
    
    Bug: T251058

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 import hashlib
 import json
 import time
+from secrets import token_hex
 
 import requests
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 clients = ['http://jitsi.meet.eqiad.wmflabs:4000']
+tokens_file = 'tokens.json'
 
-def auth(password):
+
+def auth_ticketmaster(password):
     time.sleep(2)
     with open('token', 'r') as f:
         ticketmaster_token = f.read().strip()
@@ -17,17 +20,56 @@ def auth(password):
     dk = hashlib.pbkdf2_hmac('sha256', bytes(password, 'utf-8'), salt, 100000)
     return dk.hex() == ticketmaster_token
 
+
+def auth_token(token):
+    time.sleep(2)
+    with open(tokens_file, 'r') as f:
+        tokens = json.loads(f.read())
+    if token not in tokens:
+        return False
+    tokens.remove(token)
+    with open(tokens_file, 'w') as f:
+        f.write(json.dumps(tokens))
+    return True
+
+
+def gen_token():
+    with open(tokens_file, 'r') as f:
+        tokens = json.loads(f.read())
+    token = token_hex(32)
+    tokens.append(token)
+    with open(tokens_file, 'w') as f:
+        f.write(json.dumps(tokens))
+    return token
+
+
 @app.route("/")
 def hello():
     return "Hello!"
+
+
+@app.route("/generate_token", methods=['GET'])
+def generate_token():
+    return render_template('generate.html')
+
+
+@app.route("/generate_token", methods=['POST'])
+def generate_token_post():
+    if not auth_ticketmaster(request.form['token'].strip()):
+        time.sleep(10)
+        return 'Not allowed'
+    return gen_token()
+
 
 @app.route("/create", methods=['GET'])
 def create_user():
     return render_template('create.html')
 
+
 @app.route("/create", methods=['POST'])
 def create_user_post():
-    if not auth(request.form['token'].strip()):
+    if not auth_token(request.form['token'].strip()):
+        time.sleep(10)
         return 'Not allowed'
     for client in clients:
         requests.post(
@@ -38,5 +80,7 @@ def create_user_post():
             }
         )
     return 'Done!'
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
