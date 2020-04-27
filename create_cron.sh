2d9d556be12fd9d2ea776983f7a888f4d1ef51bd
Amir Sarabadani <Ladsgroup@gmail.com>
2020-04-27 10:04:38 +0200

    Put user and pass in cron bash file inside quote
    It would break if username or password contains space

--- create_cron.sh
+++ create_cron.sh
@@ -4,7 +4,7 @@ for row in $(jq -c '.[]' /srv/meet-auth/users_to_create.json); do
     _jq() {
        echo ${row} | jq -r ${1}
     }
-    sudo docker-compose exec -T prosody prosodyctl --config /config/prosody.cfg.lua register $(_jq '.user') meet.jitsi $(_jq '.password')
+    sudo docker-compose exec -T prosody prosodyctl --config /config/prosody.cfg.lua register "$(_jq '.user')" meet.jitsi "$(_jq '.password')"
 done
 
 echo '[]' > /srv/meet-auth/users_to_create.json
